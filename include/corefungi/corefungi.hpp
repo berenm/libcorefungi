9543d7ffb1dc1313a7fc472623d9c354ac7ab750
Beren Minor <beren.minor+git@gmail.com>
2013-02-03 23:36:08 +0100

    Add LICENSE and initial source files.

---
+++ include/corefungi/corefungi.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#ifndef __COREFUNGI_COREFUNGI_HPP__
+#define __COREFUNGI_COREFUNGI_HPP__
+
+namespace corefungi {}
+
+#endif // ifndef __COREFUNGI_COREFUNGI_HPP__
